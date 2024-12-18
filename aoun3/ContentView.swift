f336a89a95ed59d7885130371ce0ab16c6545a1b
RashaAL98 <Rashaalmohammedi01@gmail.com>
2024-12-18 18:55:23 +0300

    Initial Commit

---
+++ aoun3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aoun3
+//
+//  Created by rasha on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aoun3/aoun3App.swift
@@ -0,0 +1,17 @@
+//
+//  aoun3App.swift
+//  aoun3
+//
+//  Created by rasha on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct aoun3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
